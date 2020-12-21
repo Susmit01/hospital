182db932a26c0350e4198cf495f65da06a1c498a
Susmita Mishra <heysusmita@gmail.com>
2020-12-21 23:34:08 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+include("header.php");
+?>
+<div class="wrapper col1">
+  <div id="container">
+    <div id="content">
+      <h1>Welcome to Online Appointment System </h1>
+      <p>  The goal of the Doctor appointment SYSTEM is to improve the conventional process of fixing a time with your doctor to a hassle free process and keep your health as a priority.
+.</p>
+      <p>The main feature of Online Doctor appointment system is to provide the browser to get appointments from a doctor through internet instead of going there and fixing an appointment.</p>
+      <p> Everyone needs to have Medical attention at any time. So we allow every user to register freely at any time..</p>
+      <p>We also maintain patients prescriptions so that their medical details are always available on Internet, which will be more convenient for the patients.</p>
+
+      <div class="homecontent">
+        <ul>
+          <li>
+            <h2>Book your Appointment digitally   </h2>
+            <p class="imgholder"><a href="patientappointment.php"><img src="images/appointment.png" alt="" style="width:286px;height:100px;" /></a></p>
+          </li>
+          <li class="last">
+            <h2>Login Panel for existing users</h2>          
+            <p class="imgholder"><a href="patientlogin.php"><img src="images/login.png" alt="" style="width:286px;height:100px;"  /></a></p>
+          </li>
+        </ul>
+        <div class="clear"></div>
+      </div>
+    </div>
+   
+    <div class="clear"></div>
+  </div>
+</div>
+<?php
+include("footer.php");
+?>
